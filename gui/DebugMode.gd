829b4297579979f9cc83934c0af1edeeaaee9b51
kingoftheconnors <Connormcp777@gmail.com>
2020-12-29 23:01:24 -0500

    Controller support and remove useless debug mode

--- gui/DebugMode.gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-func _ready():
-	visible = Constants.DEBUG_MODE
-
-func _unhandled_input(event):
-	if event.is_action_pressed("toggle_debug_mode"):
-		Constants.DEBUG_MODE = !Constants.DEBUG_MODE
-		visible = Constants.DEBUG_MODE
