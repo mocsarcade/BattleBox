7120d44233b4ce9236ba2fd63f8185dfdf42f0cc
kingoftheconnors <Connormcp777@gmail.com>
2020-12-30 22:29:49 -0500

    Make Camera Zoom-out when players are far apart

--- common/Camera.gd
+++ common/Camera.gd
@@ -6,25 +6,40 @@ export(NodePath) var player2 = "."
 onready var focal_point_1 = get_node(player1)
 onready var focal_point_2 = get_node(player2)
 var active = false
+var limits_l = -1000; var limits_r = 1000
+var limits_t = -1000; var limits_b = 1000
 
 func set_level(new_level : Node2D):
 	if new_level:
-		limit_top = new_level.up
-		limit_bottom = new_level.down
-		limit_left = new_level.left
-		limit_right = new_level.right
+		limits_t = new_level.up
+		limits_b = new_level.down
+		limits_l = new_level.left
+		limits_r = new_level.right
 		drag_margin_h_enabled = true
 		drag_margin_v_enabled = true
 		smoothing_enabled = true
 	else:
-		limit_top = -1000
-		limit_bottom = 1000
-		limit_left = -1000
-		limit_right = 1000
+		limits_t = -1000
+		limits_b = 1000
+		limits_l = -1000
+		limits_r = 1000
 		drag_margin_h_enabled = false
 		drag_margin_v_enabled = false
 		position = Vector2.ZERO
 		smoothing_enabled = false
 
+onready var base_zoom = self.zoom
+onready var zoom_goal = base_zoom
+
 func _process(delta):
 	global_position = (focal_point_1.global_position + focal_point_2.global_position)/2
+	# If we are in a level
+	if smoothing_enabled:
+		var distance = focal_point_1.global_position.distance_to(focal_point_2.global_position)
+		var zoom_factor = distance * 0.0018
+		zoom_goal = Vector2.ONE * clamp(zoom_factor, 0.5, .70)
+		zoom = lerp(zoom, zoom_goal, 0.02)
+		limit_bottom = limits_b; limit_top = limits_t * (zoom.y/base_zoom.y)
+		limit_left = limits_l * (zoom.x/base_zoom.x); limit_right = limits_r * (zoom.x/base_zoom.x)
+	else:
+		zoom = base_zoom
